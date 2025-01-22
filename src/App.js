4bdb59c2316377f23d2ab65abb90c86034e530a2
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-01-22 17:27:55 +0530

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,71 +1,10 @@
 import './App.css';
-import StopWatch from './Component/0.test/StopWatch';
-import Test1 from './Component/0.test/Test1';
-import Watch from './Component/0.test/Watch';
 import Hello from './Component/1.Basic/1.HelloWord/Hello';
-import CountApp from './Component/1.Basic/2.CountApp/CountApp';
-import Studentlist from './Component/1.Basic/3.ListComponent/Studentlist';
-import Conditional from './Component/1.Basic/4.ConditionalRendering/Conditional';
-import MultipleState from './Component/1.Basic/5.MultipleState/MultipleState';
-import SingleState from './Component/1.Basic/5.MultipleState/SingleState';
-import BasicRef from './Component/10.UseRef/1.Basic/BasicRef';
-import InputComponentForm from './Component/10.UseRef/2.Imp/InputComponentForm';
-import ShowLatestValAndPrevVal from './Component/10.UseRef/2.Imp/ShowLatestValAndPrevVal';
-import SimpleFormInputBoxComponent from './Component/10.UseRef/2.Imp/SimpleFormInputBoxComponent';
-import UndoMessage from './Component/10.UseRef/2.Imp/UndoMessage';
-import CounterReducer from './Component/11.UseReduce/CounterReducer';
-import Parent from './Component/12.Performance/1.Re-rendering/1.NoProps/Parent';
-import ParentVP from './Component/12.Performance/1.Re-rendering/2.ValueProps/ParentVP';
-import ParentFP from './Component/12.Performance/1.Re-rendering/3.FunctionalProps/ParentFP';
-import Redux from './Component/13.Redux/Redux';
-import ParentPTC from './Component/2.Communication/1.ParentToChild/ParentPTC';
-import ParentCTP from './Component/2.Communication/2.ChildToParent/ParentCTP';
-import ParentS from './Component/2.Communication/3.Sibling/ParentS';
-import KeysAndListProblem from './Component/3.KeysAndList/KeysAndListProblem';
-import KeysAndListSouction from './Component/3.KeysAndList/KeysAndListSouction';
-import HookUseEffect from './Component/5.HookUseEffect/HookUseEffect';
-import DigitalClock from './Component/6.DigitalClock/DigitalClock';
-import CounterClassComponent from './Component/7.ClassComponent/2.Counter/Count';
-import ReactRouter from './Component/8.ReactRouting/ReactRouter';
-import MainContext from './Component/9.ContextApi/1.Normal/Component/MainContext';
-import CREDwithLo from './Component/9.ContextApi/3.CRED/CREDwithLo';
-import Pr from "./Component/0.test/Pr"
-
 
 function App() {
   return (
     <div className="App">
-      {/* <Hello/> */}
-      {/* <CountApp/> */}
-      {/* <Studentlist/> */}
-      {/* <Conditional/> */}
-      {/* <SingleState/> */}
-      {/* <MultipleState/> */}
-      {/* <ParentPTC/> */}
-      {/* <ParentCTP/> */}
-      {/* <ParentS/> */}
-      {/* <KeysAndListProblem/> */}
-      {/* <KeysAndListSouction/> */}
-      {/* <HookUseEffect/> */}
-      {/* <DigitalClock/> */}
-      {/* <CounterClassComponent/> */}
-      {/* <ReactRouter/> */}
-      {/* <MainContext/> */}
-      {/* <CREDwithLo/> */}
-      {/* <BasicRef/> */}
-      {/* <ShowLatestValAndPrevVal/> */}
-      {/* <SimpleFormInputBoxComponent/> */}
-      {/* <InputComponentForm/> */}
-      {/* <UndoMessage/> */}
-      {/* <CounterReducer/> */}
-      {/* <Parent/> */}
-      {/* <ParentVP/> */}
-      {/* <ParentFP/> */}
-      {/* <Redux/> */}
-      {/* <Watch/> */}
-      {/* <Test1/> */}
-      {/* <StopWatch/> */}
-      <Pr/>
+      <Hello/>
     </div>
   );
 }
