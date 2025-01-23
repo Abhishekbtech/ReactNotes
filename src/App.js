47027bceb2907110ab251afc1a73a5ce063d99fb
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-01-23 14:03:09 +0530

    add class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HelloClassComponent from './Component/1.Basic/1.HelloWord/Class';
 import Hello from './Component/1.Basic/1.HelloWord/Hello';
 
 function App() {
   return (
     <div className="App">
       <Hello/>
+      <HelloClassComponent/>
     </div>
   );
 }
