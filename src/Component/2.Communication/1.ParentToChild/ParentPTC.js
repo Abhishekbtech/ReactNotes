2d8f774711e31b7bc74da9a0dc96bf903f8e034c
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-01-26 11:50:15 +0530

    add new

--- src/Component/2.Communication/1.ParentToChild/ParentPTC.js
+++ src/Component/2.Communication/1.ParentToChild/ParentPTC.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChildPTC from './ChildPTC'
 
 function ParentPTC() {
-    const infomation = "i am getting value from Parent"
+    const [showChild, setShowChild] = useState(false)
+    const information = "i am getting value from Parent"
+
+    const handleClick = () => {
+        setShowChild(!showChild)
+    }
+
     return (
         <>  
             <h1>Parent to Child Communication</h1>
-            <ChildPTC info={infomation}/>
+            <button onClick={handleClick}>
+                {showChild ? 'Hide Child' : 'Show Child'}
+            </button>
+            {showChild && <ChildPTC info={information}/>}
         </>
     )  
 }
