e23293bdb9c3968837bdff36d02bca693f2bae59
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-01-29 11:46:19 +0530

    update no page found

--- src/Component/8.ReactRouting/5.NoPageFound/NoPageFound.js
+++ src/Component/8.ReactRouting/5.NoPageFound/NoPageFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function NoPageFound() {
     return (
         <>
-            <h1>No Page Found</h1>
+            <h2>No Page Found</h2>
             <Link to="/">
                 <button>Go to Home</button>
             </Link>
