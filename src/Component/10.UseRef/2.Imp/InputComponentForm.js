8c521283b965ecbf392c3b8107061f42fecb0f18
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-01-31 16:35:19 +0530

    add

--- src/Component/10.UseRef/2.Imp/InputComponentForm.js
+++ src/Component/10.UseRef/2.Imp/InputComponentForm.js
@@ -1,4 +1,4 @@
-import React,{ useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Input from './Input';
 
 function InputComponentForm() {
@@ -12,8 +12,8 @@ function InputComponentForm() {
     console.log(inputBoxReference, "2");
     return (
         <>
-        <h1>Input component form</h1>
-        <Input ref={inputBoxReference} asd={inputBoxReference} />
+            <h1>Input component form</h1>
+            <Input ref={inputBoxReference} asd={inputBoxReference} />
         </>
     );
 }
