f230b954a8c9029d7ebb573324a288f6b4407ed0
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2025-02-02 11:24:16 +0530

    add use Memo example

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import LocalStorage from './Component/1.Basic/6.DataStorage/LocalStorage';
 import SessionStorage from './Component/1.Basic/6.DataStorage/SessionStorage';
 import BasicRef from './Component/10.UseRef/1.Basic/BasicRef';
 import CounterReducer from './Component/11.UseReduce/CounterReducer';
+import ParentPer from './Component/12.Performance/1.Re-rendering/1.NoProps/ParentPer';
 import ParentPTC from './Component/2.Communication/1.ParentToChild/ParentPTC';
 import ParentCTP from './Component/2.Communication/2.ChildToParent/ParentCTP';
 import ParentS from './Component/2.Communication/3.Sibling/ParentS';
@@ -66,6 +67,9 @@ function App() {
 
 
       <CounterReducer/>
+
+
+      <ParentPer/>
     </div>
   );
 }
--- src/Component/12.Performance/1.Re-rendering/1.NoProps/ParentPer.js
+++ src/Component/12.Performance/1.Re-rendering/1.NoProps/ParentPer.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import Child from './Child';
 
-function Parent() {
+function ParentPer() {
     const [count, setCount] = useState(0)
     console.log("parent rendering");
     return (
         <>
-            <h1>Parent</h1>
+            <h1>Performance use Memo</h1>
             <h2>Count: {count}</h2>
             <button onClick={() => setCount((old) => old + 1)}>Click me</button>
             <Child/>
@@ -15,4 +15,4 @@ function Parent() {
     )
 }
 
-export default Parent
+export default ParentPer
